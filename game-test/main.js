f2b2bfe9078e880d81f75e5506af55fc165a34ce
lucaspoor <76880415+lucaspoor@users.noreply.github.com>
2021-01-08 18:46:50 -0300

    Add files via upload
    :)

---
+++ game-test/main.js
@@ -0,0 +1,32 @@
+var character = document.getElementById("character")
+var block = document.getElementById("block")
+
+function jump() {   
+
+   
+    if (character.classList != "animate"){     
+    character.classList.add("animate");
+}
+    setTimeout(function(){
+character.classList.remove("animate");
+    },450);
+}
+document.body.onkeypress = jump;
+
+var checkDead = setInterval(function (){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft<-205 && blockLeft>-272 && characterTop>=53){
+
+        block.style.animation = "none";
+        alert("you lose!!!")
+    }
+},10)
+
+
+
+
+
+
+
+
